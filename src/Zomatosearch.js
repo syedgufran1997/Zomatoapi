ccd5153b20080e0fedc9a61bde3f060a603ee90c
syedgufran1997 <syedgufran1997@gmail.com>
2020-05-13 06:44:34 +0530

    retaurant update on searchbar

--- src/Zomatosearch.js
+++ src/Zomatosearch.js
@@ -3,29 +3,98 @@ import zomatosearch from "./zomatosearch.module.css";
 
 class Zomatosearch extends Component {
   state = {
-    loading: true,
+    categories: [],
+    latitude: "",
+    longitude: "",
+    searchResult: [],
+    reviews: [],
   };
-  async componentDidMount() {
-    const url =
-      "https://developers.zomato.com/api/v2.1/cities?q=Bangalore&city_ids=50&count=50";
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
+
+  componentDidMount() {
+    this.getCurrectLocation();
   }
+
+  getCurrectLocation = () => {
+    window.navigator.geolocation.getCurrentPosition((position) => {
+      this.setState(
+        {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        },
+        () => console.log(this.state.latitude, this.state.longitude)
+      );
+    });
+  };
+
+  searchRestaurants = (searchText) => {
+    fetch(
+      `https://developers.zomato.com/api/v2.1/search?q=${searchText}&count=5&lat=${this.state.latitude}&lon=${this.state.longitude}&sort=real_distance&order=asc
+    `,
+      {
+        headers: {
+          "user-key": "8fa87ac61412b80b53536a2aa6e45648",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState(
+          { searchResult: data.restaurants, reviews: [] },
+          () => console.log(this.state.searchResult),
+          document.write(this.state.searchResult)
+        )
+      )
+      .catch((err) => console.log(err));
+  };
+
+  getReview = (id) => {
+    fetch(`https://developers.zomato.com/api/v2.1/reviews?res_id=${id}`, {
+      headers: {
+        "user-key": "8fa87ac61412b80b53536a2aa6e45648",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({ searchResult: [], reviews: data.user_reviews }, () =>
+          console.log(this.state.reviews)
+        )
+      )
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
       <div className={zomatosearch.input}>
-        <h1>Zomato</h1>
+        <h1>zomato</h1>
         <h3>Discover food and drinks in your place.</h3>
         <label className={zomatosearch.searchlabel} htmlFor="search-input">
           <input
             type="text"
-            name="Search"
-            placeholder="Search for Restaurent or dish."
+            onChange={(e) => this.searchRestaurants(e.target.value)}
+            placeholder="enter restaurant name..."
           />
           <i className={`fa fa-search ${zomatosearch.searchicon}`}></i>
         </label>
-        {this.state.loading ? <div>loading...</div> : <div>person..</div>}
+        {this.state.searchResult.length &&
+          this.state.searchResult.map(({ restaurant }) => (
+            <div
+              onClick={() => this.getReview(restaurant.id)}
+              key={restaurant.id}
+            >
+              {restaurant.name} - {restaurant.location.address}
+            </div>
+          ))}
+        {this.state.reviews.length > 0 &&
+          this.state.reviews.map(({ review }) => {
+            return (
+              <div className={zomatosearch.reviews}>
+                <h2>Review - {review.review_text + review.rating_text}</h2>
+                <h3>Rating -{review.rating}</h3>
+                <h4>User - {review.user.name}</h4>
+              </div>
+            );
+          })}
+        }
       </div>
     );
   }
